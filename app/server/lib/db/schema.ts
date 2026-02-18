a3dea110ec618c9f8b43846fd5636af005335f8b
gbrehault <gabriel032156@gmail.com>
2026-02-18 10:56:29 +0100

    Refactor database schema: move table definitions to schema-definition directory and remove old files

--- app/server/lib/db/schema.ts
+++ app/server/lib/db/schema.ts
@@ -1,20 +1,20 @@
 // better-auth schema
-export { user, session, account, verification } from './auth-schema';
+export { user, session, account, verification } from './schema-definition/auth-schema';
 
 // teacher schema
-export { teacher } from './teacher';
+export { teacher } from './schema-definition/teacher';
 
 // learner schema
-export { learner } from './learner';
+export { learner } from './schema-definition/learner';
 
 // availability schema
-export { availability } from './availability';
+export { availability } from './schema-definition/availability';
 
 // booking schema
-export { booking } from './booking';
+export { booking } from './schema-definition/booking';
 
 // course schema
-export { course } from './course';
+export { course } from './schema-definition/course';
 
 // rating schema
-export { rating } from './rating';
+export { rating } from './schema-definition/rating';
