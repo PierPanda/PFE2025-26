7cf2d535a9d80f589c75feecd933918c56e1562d
Loïc Souêtre <loic.souetre@sfr.fr>
2026-02-25 21:23:24 +0100

    feat(dashboard): add CourseCard component and update dashboard

--- app/routes/pages/dashboard/page.tsx
+++ app/routes/pages/dashboard/page.tsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router";
 import type { LoaderFunctionArgs } from "react-router";
 import { authentifyUser } from "~/server/utils/authentify-user.server";
+import type { Route } from "./+types/page";
+import { useLoaderData } from "react-router";
+import CourseCard from "~/components/ui/CourseCard";
 
-export async function loader({ request }: LoaderFunctionArgs) {
+export async function loader({ request }: Route.LoaderArgs) {
   await authentifyUser(request, { redirectTo: "/auth" });
-  return {};
+  let courses = [];
+  try {
+    const apiUrl = new URL("/api/courses", request.url);
+    const result = await fetch(apiUrl).then((res) => res.json());
+    courses = result.courses;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des cours :", error);
+    return { user: null, courses: [] };
+  }
+  return { courses };
 }
 
 export function meta() {
@@ -15,6 +27,8 @@ export function meta() {
 }
 
 export default function Home() {
+  const { courses } = useLoaderData();
+  console.log("Courses reçues dans Home :", courses);
   return (
     <main className="mx-auto max-w-7xl px-6 py-10">
       <div className="rounded-2xl bg-brand/10 p-8 shadow-sm">
@@ -27,6 +41,11 @@ export default function Home() {
           + Créer un cours
         </Link>
       </div>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {courses.map((course: any) => (
+          <CourseCard key={course.id} course={course} />
+        ))}
+      </ul>
     </main>
   );
 }
