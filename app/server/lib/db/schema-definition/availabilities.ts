ffa2be7e74b355c6d7264e99351682a5f7d34bc7
PierPanda <160773339+PierPanda@users.noreply.github.com>
2026-04-02 16:23:55 +0200

    feat(availabilities): ajouter la gestion des exceptions pour les créneaux horaires

--- app/server/lib/db/schema-definition/availabilities.ts
+++ app/server/lib/db/schema-definition/availabilities.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { boolean, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 import { teachers } from './teachers';
 
 export const availabilities = pgTable('availabilities', {
@@ -8,6 +8,8 @@ export const availabilities = pgTable('availabilities', {
     .references(() => teachers.id),
   startTime: timestamp('startTime').notNull(),
   endTime: timestamp('endTime').notNull(),
+  isException: boolean('isException').notNull().default(false),
+  exceptionReason: text('exceptionReason'),
   createdAt: timestamp('createdAt').notNull().defaultNow(),
   updatedAt: timestamp('updatedAt').notNull().defaultNow(),
 });
