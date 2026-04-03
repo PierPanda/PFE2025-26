04e5b6742bf826c85ef3f1363c5257bc70fd06ce
PierPanda <160773339+PierPanda@users.noreply.github.com>
2026-04-03 09:20:00 +0200

    feat(database): ajouter les colonnes isException et exceptionReason dans availabilities

--- drizzle/0004_right_human_fly.sql
+++ drizzle/0004_right_human_fly.sql
@@ -4,4 +4,5 @@ CREATE TYPE "public"."category" AS ENUM('piano', 'guitare', 'violon', 'batterie'
 ALTER TABLE "courses" ALTER COLUMN "category" SET DATA TYPE "public"."category" USING "category"::"public"."category";--> statement-breakpoint
 ALTER TABLE "user" ALTER COLUMN "role" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "user" ALTER COLUMN "banned" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "availabilities" ADD COLUMN "isException" boolean DEFAULT false NOT NULL;
+ALTER TABLE "availabilities" ADD COLUMN "isException" boolean DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE "availabilities" ADD COLUMN "exceptionReason" text;
